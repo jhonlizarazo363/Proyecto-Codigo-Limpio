ad05e93716d8fb9f468208413121edba73ae567f
Juan Yepes <yepesvalenciajuandiego@gmail.com>
2026-03-02 12:29:02 -0500

    initial .json

---
+++ src/mi_app/storage.py
@@ -0,0 +1,9 @@
+import json
+from pathlib import Path
+from typing import TypeVar, Type, List
+from dataclasses import asdict
+
+from .models import Vehiculo, Cliente, Alquiler
+
+T = TypeVar("T")
+
